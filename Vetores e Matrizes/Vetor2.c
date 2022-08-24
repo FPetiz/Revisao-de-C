b8eee84794105d981ff0b7c0d2c8fbbd8bc39583
Fernanda <101828371+FPetiz@users.noreply.github.com>
2022-08-23 22:56:30 -0300

    Update Vetor2.c

--- Vetores e Matrizes/Vetor2.c
+++ Vetores e Matrizes/Vetor2.c
@@ -14,19 +14,19 @@
         printf("Digite o %d° inteiro: ", i+1);
         scanf("%d", &vetor[i]);
 
-        if(vetor[i] % 2 == 0){
-            soma += vetor[i];}
+        if(vetor[i] % 2 == 0){  // Verifica se o conteúdo de vetor[i] é par
+            soma += vetor[i];}  // Se for par soma o conteúdo da posição à variável soma
         else{
-            impares += 1;}
+            impares += 1;}      // Se for ímpar, incrementa o contador de ímpares
     }
     printf("\nOs numeros pares recebidos sao:\n");
-    for(i = 0; i <= 5; i++){
-        if(vetor[i] % 2 == 0){
+    for(i = 0; i <= 5; i++){  
+        if(vetor[i] % 2 == 0){  // Verifica, dos 5 números, quais são pares para imprimí-los
             printf("\t%d", vetor[i]);}
     } printf("\nE a soma deles eh: %d", soma);
     printf("\nForam recebidos %d numeros impares, e eles sao:\n", impares);
     for(i = 0; i <= 5; i++){
-        if(vetor[i] % 2 == 1){
+        if(vetor[i] % 2 == 1){  // Verifica, dos 5 números, quais são ímpares
             printf("\t%d", vetor[i]);}
     }
     return 0;
